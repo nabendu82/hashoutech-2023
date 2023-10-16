4dc51ff1d3e4a6fbd7992a7ff47560c2d5149d38
nabendu82 <nabendu.biswas@gmail.com>
2023-10-16 09:09:02 +0530

    JavaScript started

---
+++ JavaScript/main.js
@@ -0,0 +1,44 @@
+//Variables
+// var firstName = 'Nabendu';
+// let age = 41;
+// let rating = 5.5;
+// const isDeveloper = true;
+// isDeveloper = false;
+
+// console.log(firstName)
+
+// function varExp() {
+//     if(true) {
+//         var num1 = 10;
+//         console.log("Inside if var " + num1);
+//     }
+//     console.log("Outside if var " + num1);
+// }
+
+// varExp();
+// console.log("Outside function var " + num1);
+
+// if(true) {
+//     let num2 = 20;
+//     console.log("Inside if let " + num2);
+// }
+// console.log("Outside if let " + num2);
+
+//Datatypes
+const firstName = 'Nabendu';
+const age = 41;
+const rating = 5.5;
+const isDev = true;
+const x = null;
+let z;
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof isDev);
+console.log(typeof x);
+if(typeof x === 'object'){
+    console.log("Do something with this null")
+}
+console.log(typeof z);
+
