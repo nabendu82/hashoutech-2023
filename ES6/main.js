b24569650e0bbd1e531554827e51074433f1edd6
nabendu82 <nabendu.biswas@gmail.com>
2023-10-31 09:03:57 +0530

    ES6 done

--- ES6/main.js
+++ ES6/main.js
@@ -78,17 +78,118 @@
 // console.log(c);
 
 //Spread operator to merge arrays
-let x1 = [1, 2], a1 = [3, 4], c1 = [9, 10];
-let d1 = [...x1, ...a1, ...c1];
-console.log(d1);
+// let x1 = [1, 2], a1 = [3, 4], c1 = [9, 10];
+// let d1 = [...x1, ...a1, ...c1];
+// console.log(d1);
 
 //Used in Objects
-let obj1 = { foo: 'bar', x: 42 }, obj2 = { foo: 'baz', y: 13 };
-let clonedObj = { ...obj1 };
-clonedObj.z = 45;
+// let obj1 = { foo: 'bar', x: 42 }, obj2 = { foo: 'baz', y: 13 };
+// let clonedObj = { ...obj1 };
+// clonedObj.z = 45;
 // console.log(obj1);
-console.log(clonedObj);
-let mergedObj = { ...obj1, ...obj2, ...clonedObj };
-console.log(mergedObj);
+// console.log(clonedObj);
+// let mergedObj = { ...obj1, ...obj2, ...clonedObj };
+// console.log(mergedObj);
 
+//Array Destructuring
+// const rgb = [255, 200, 0];
 
+// const r = rgb[0];
+// const g = rgb[1];
+// const b = rgb[2];
+
+// const [r, g, b] = rgb;
+// console.log(r, g, b);
+
+//Object destructuring
+// const developer = {
+//     firstname: 'Nabendu',
+//     lastname: 'Biswas',
+//     country: 'India'
+// };
+
+// const firstname = developer.firstname;
+// const lastname = developer.lastname;
+// const country = developer.country;
+
+// const { firstname, lastname, country } = developer;
+// console.log(firstname, lastname, country);
+
+//ES6 classes
+// class CreateRoom {
+//     constructor(name) {
+//         this.room = `${name}'s room`
+//     }
+
+//     cleanRoom(soap){
+//         console.log(`Cleaning ${this.room} with ${soap}`)
+//     }
+// }
+
+// const nabsRoom = new CreateRoom('Nabendu');
+// const hridayRoom = new CreateRoom('Hriday');
+
+// nabsRoom.cleanRoom('Domex');
+// hridayRoom.cleanRoom('Lizol');
+
+//Classical for loop
+// let developers = ['Nabendu', 'Hari', 'Chetan'];
+// for(let i=0; i<developers.length; i++){
+//     console.log(developers[i]);
+// }
+
+//forEach
+// developers.forEach(person => console.log(person));
+
+//Map use case
+// let numbers = [6, 8, 10];
+
+// let doubled = [];
+// for(let i=0; i<numbers.length; i++){
+//     doubled.push(numbers[i]*2);
+// }
+
+// console.log(doubled)
+
+//Map
+// let doubledES6 = numbers.map(num => num*2);
+// console.log(doubledES6);
+
+//filter use case
+let products = [
+    { name: 'Samsung M21', category: 'smartphone'},
+    { name: 'Samsung M31', category: 'smartphone'},
+    { name: 'Macbook Air', category: 'laptop'},
+    { name: 'Asus Zenbook', category: 'laptop'}
+];
+
+// let filtered = [];
+// for(let i=0; i<products.length; i++){
+//     if(products[i].category === 'smartphone'){
+//         filtered.push(products[i])
+//     }
+// }
+
+// console.log(filtered);
+
+//filter
+let filterES6 = products.filter(item => item.category === 'laptop');
+console.log(filterES6);
+
+//Classic use case of reduce
+let numbers = [20, 30, 40];
+
+// var sum = 0;
+// for(let i=0; i<numbers.length; i++){
+//     sum += numbers[i];
+// } 
+
+// console.log(sum);
+
+const sumReduce = numbers.reduce((acc, curr) => {
+    return acc + curr;
+}, 0)
+//0 + 20 = 20
+//20 + 30 = 50
+//50 + 40 = 90
+console.log(sumReduce);
